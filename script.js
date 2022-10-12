b23031b09036ab62ba87479a054d12c71be7b4df
olubiyi09 <olubiyi09@gmail.com>
2022-10-12 22:38:53 +0400

    added dark and light mode to the project

--- script.js
+++ script.js
@@ -10,6 +10,44 @@ let moviesDiv = document.querySelector(".movies");
 let form = document.querySelector("form");
 let input = document.querySelector(".search");
 
+//////////////////////////////////
+//Activate DarkMode
+const toggleDarkMode = document.querySelector(".toggle-dark");
+const toggleTxt = document.querySelector(".txt");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Enable Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleTxt.textContent = "light";
+  localStorage.setItem("darkMode", "enabled");
+};
+
+// Disable Dark Mode
+const disableDarkMode = () => {
+  document.body.classList.remove("darkmode");
+  toggleTxt.textContent = "dark";
+  localStorage.setItem("darkMode", null);
+};
+
+// Save DarkMode History
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+// Add Event Listener
+toggleDarkMode.addEventListener("click", () => {
+  let darkMode = localStorage.getItem("darkMode");
+
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+/////////////////////////////////////
+
 getMovies(apiUrl);
 async function getMovies(url) {
   const response = await fetch(url);
